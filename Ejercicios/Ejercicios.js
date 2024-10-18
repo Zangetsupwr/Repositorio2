ef2d72da47258463d1d0a609755f76beb795e25b
Zangetsupwr <powermirsson@gmail.com>
2024-10-18 17:24:37 -0300

    Add: Update
    Se actualiza codigo de ejercicios

---
+++ Ejercicios/Ejercicios.js
@@ -0,0 +1,4 @@
+/* Imprimir pares del 1 al 30: Usando un bucle, escribe un código que imprima todos los números pares del 1 al 30. ¡Veamos si puedes resolverlo!*/
+for (var i=2; i<=30; i+=2){
+        console.log("Numeros Pares :"+i);
+   }
---
+++ Octubre-17/script.js
@@ -0,0 +1,24 @@
+function handleClick(elemento) {
+    console.log(elemento);
+    elemento.style.backgroundColor = "red";
+}
+
+function handleMouseOver(elemento) {
+    console.log(elemento);
+    //Cuando mi mouse esta dentro del evento creo un evento que permite escuchar el movimiento del mouse
+    let scale = 1;
+    elemento.addEventListener('wheel',(e) =>{
+        console.log(e);
+        console.log(e.wheelDelta);
+        //Cambiar el tamaño del elemento
+
+        if (e.wheelDelta < 0){
+            scale -=0.1;
+            elemento.style.transform =`scale(${scale})`;
+        }
+        else(e.wheelDelta > 0){
+            scale +=0.1;
+            elemento.style.transform =`scale(${scale})`;
+        }
+    }   
+)}
